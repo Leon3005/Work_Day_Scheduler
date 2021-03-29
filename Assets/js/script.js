2717105bb74e0a0678bcae1988ab299794f637ba
Leon <leonwheeler08@gmail.com>
2021-03-29 22:35:06 +0100

    Added comments

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,15 +1,14 @@
-//Use moment to input date dynamically
+//Use moment to input date dynamically on page ready
 $(document).ready(function () {
   const todaysDate = moment();
   $("#currentDay").text(todaysDate.format("dddd Do MMMM, YYYY"));
 });
 
-const dayHours = [];
-
 //For loop to create timeblocks.
-//Need to use 'hour' for moment js to recognise as a time
+//Need to use 'hour' for moment js to recognise as a time.
 for (let hour = 8; hour < 18; hour++) {
   $(".container").append(
+    //This row will contain 3 columns, but all in the same div. One for time, one for activity, one for the save button.
     `<div class="row time-block">${moment({ hour }).format("h A")}</div>`
   );
 }
