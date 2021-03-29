00092ff2cccdbc952ec4bdc0719b7b4c2703b99d
Leon <leonwheeler08@gmail.com>
2021-03-29 21:26:26 +0100

    Created a for loop to create time block divs

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -3,3 +3,13 @@ $(document).ready(function () {
   const todaysDate = moment();
   $("#currentDay").text(todaysDate.format("dddd Do MMMM, YYYY"));
 });
+
+const dayHours = [];
+
+//For loop to create timeblocks.
+//Need to use 'hour' for moment js to recognise as a time
+for (let hour = 8; hour < 18; hour++) {
+  $(".container").append(
+    `<div class="row time-block">${moment({ hour }).format("h A")}</div>`
+  );
+}
