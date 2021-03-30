f8290b4d0c7fb31864b857a874fa438a1191322d
Leon <leonwheeler08@gmail.com>
2021-03-30 15:42:35 +0100

    Trying to change background colour depending on past/present. Only 1 block changes

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,20 +6,28 @@ $(document).ready(function () {
 
 //For loop to create timeblocks.
 //Need to use 'hour' for moment js to recognise as a time.
-for (let hour = 8; hour <= 17; hour++) {
-  $(".container").append(
-    //This row will contain 3 columns, but all in the same div. One for time, one for activity, one for the save button. May need to use bootstrap classes
-    // `<div class="row time-block">${moment({ hour }).format("h A")}</div>`
-    `<div class="row">
-      <div class="col- time-block hour mr-4">
-        ${moment({ hour }).format("h A")}
-      </div>
-      <div class="col-md">
-        One of three columns
-      </div>
-      <div class="col-">
-        Btn here
-      </div>
-    </div>`
-  );
-}
+const timeLoop = () => {
+  for (let hour = 8; hour <= 17; hour++) {
+    $(".container").append(
+      //This row will contain 3 columns, but all in the same div. One for time, one for activity, one for the save button. May need to use bootstrap classes
+      // `<div class="row time-block">${moment({ hour }).format("h A")}</div>`
+      `<div class="row">
+        <div class="col- time-block hour mr-4">
+          ${moment({ hour }).format("h A")}
+        </div>
+        <div id="timeCheck" class="col-md">
+          One of three columns
+        </div>
+        <div class="col-">
+          Btn here
+        </div>
+      </div>`
+    );
+
+    if (hour < moment().format("H")) {
+      $("#timeCheck").addClass("past");
+    }
+  }
+};
+
+timeLoop();
